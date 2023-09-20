4037c7614d120abace4399a63ae3d9c24259ae5e
Nguyen Duc Duy (SZF_2022_2024) <nguyen.duc.duy1@diak.szamalk-szalezi.hu>
2023-09-20 12:50:37 +0200

    ciklus

--- src/konzolosalkalmazas/KonzolosAlkalmazas.java
+++ src/konzolosalkalmazas/KonzolosAlkalmazas.java
@@ -2,11 +2,10 @@ package konzolosalkalmazas;
 
 public class KonzolosAlkalmazas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+            
+        }
     }
     
 }
