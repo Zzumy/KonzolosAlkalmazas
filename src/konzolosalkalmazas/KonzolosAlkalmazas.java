62ab2589759bf91ae7f3eb0b04631a1cd8016902
Nguyen Duc Duy (SZF_2022_2024) <nguyen.duc.duy1@diak.szamalk-szalezi.hu>
2023-09-20 12:51:50 +0200

    metodus

--- src/konzolosalkalmazas/KonzolosAlkalmazas.java
+++ src/konzolosalkalmazas/KonzolosAlkalmazas.java
@@ -3,12 +3,15 @@ package konzolosalkalmazas;
 public class KonzolosAlkalmazas {
 
     public static void main(String[] args) {
+        metodus()
+        {
+        }
         for (int i = 0; i < 10; i++) {
-            
+
         }
         if (true) {
-            
+
         }
     }
-    
+
 }
